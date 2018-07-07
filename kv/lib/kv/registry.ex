cf1bbadd1305aef7160796f07a6cfae662589d7a
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-07-07 15:24:58 +0900

    gen_server (2)

--- kv/lib/kv/registry.ex
+++ kv/lib/kv/registry.ex
@@ -28,14 +28,17 @@ defmodule KV.Registry do
 
   ## Server Callbacks
 
+  @impl true
   def init(:ok) do
     {:ok, %{}}
   end
 
+  @impl true
   def handle_call({:lookup, name}, _from, names) do
     {:reply, Map.fetch(names, name), names}
   end
 
+  @impl true
   def handle_cast({:create, name}, names) do
     if Map.has_key?(names, name) do
       {:noreply, names}
