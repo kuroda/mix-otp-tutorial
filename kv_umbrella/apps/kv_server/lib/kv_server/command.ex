76a7ff389294bdaadd7dc4f7c83cca1de0a7f2cf
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-07-08 16:56:12 +0900

    running commands

--- kv_umbrella/apps/kv_server/lib/kv_server/command.ex
+++ kv_umbrella/apps/kv_server/lib/kv_server/command.ex
@@ -1,11 +1,4 @@
 defmodule KVServer.Command do
-  @doc """
-  Runs the given command.
-  """
-  def run(_command) do
-    {:ok, "OK\r\n"}
-  end
-
   @doc ~S"""
   Parses the given `line` into a command.
 
@@ -43,6 +36,44 @@ defmodule KVServer.Command do
       ["PUT", bucket, key, value] -> {:ok, {:put, bucket, key, value}}
       ["DELETE", bucket, key] -> {:ok, {:delete, bucket, key}}
       _ -> {:error, :unknown_command}
+    end
+  end
+
+  @doc """
+  Runs the given command.
+  """
+  def run(command)
+
+  def run({:create, bucket}) do
+    KV.Registry.create(KV.Registry, bucket)
+    {:ok, "OK\r\n"}
+  end
+
+  def run({:get, bucket, key}) do
+    lookup(bucket, fn pid ->
+      value = KV.Bucket.get(pid, key)
+      {:ok, "#{value}\r\nOK\r\n"}
+    end)
+  end
+
+  def run({:put, bucket, key, value}) do
+    lookup(bucket, fn pid ->
+      KV.Bucket.put(pid, key, value)
+      {:ok, "OK\r\n"}
+    end)
+  end
+
+  def run({:delete, bucket, key}) do
+    lookup(bucket, fn pid ->
+      KV.Bucket.delete(pid, key)
+      {:ok, "OK\r\n"}
+    end)
+  end
+
+  defp lookup(bucket, callback) do
+    case KV.Registry.lookup(KV.Registry, bucket) do
+      {:ok, pid} -> callback.(pid)
+      :error -> {:error, :not_found}
     end
   end
 end
