619c561d65e719e00f482e9a58eb21b27ca89498
Tsutomu Kuroda <t-kuroda@oiax.jp>
2018-07-08 13:08:55 +0900

    task and gen_tcp (3)

--- kv_umbrella/apps/kv_server/lib/kv_server/application.ex
+++ kv_umbrella/apps/kv_server/lib/kv_server/application.ex
@@ -8,7 +8,8 @@ defmodule KVServer.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
-      {Task, fn -> KVServer.accept(4040) end}
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      Supervisor.child_spec({Task, fn -> KVServer.accept(4040) end}, restart: :permanent)
     ]
 
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
